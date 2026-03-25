126f7aa7a4d28626cb6021d9ecbe1c442d76b099
Gabriela-Petcu <petcugabrielai13@gmail.com>
2026-03-25 20:13:49 +0200

    feat: add DbContext and configure database

---
+++ backend/MoodTrackerAPI/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using MoodTrackerAPI.Models;
+
+namespace MoodTrackerAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MoodEntry> MoodEntries { get; set; }
+    }
+}
